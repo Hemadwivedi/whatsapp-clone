74e766e081fe67cafe98ec84f0bcec0354cc22cf
HDwivedi <hemaasheesh@gmail.com>
2020-09-04 10:58:16 -0400

    component added

---
+++ whatsapp-clone/src/Components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Sidebar.css'
+const sideBars=()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default sideBars;
---
+++ whatsapp-clone/src/FireBase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAt_a20YBR44UWrlRIQpbuWxLbHQfT049I",
+    authDomain: "whatsapp-clone-80961.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-80961.firebaseio.com",
+    projectId: "whatsapp-clone-80961",
+    storageBucket: "whatsapp-clone-80961.appspot.com",
+    messagingSenderId: "986629369115",
+    appId: "1:986629369115:web:2765bacb7974e01dba77be",
+    measurementId: "G-S5T1J5R0TS"
+  };
